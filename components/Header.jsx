60f54653eb50a383bd5cacb74ef004470d8f3376
shan3274 <khanshan949@gmail.com>
2024-04-02 14:27:53 +0530

    add video

--- components/Header.jsx
+++ components/Header.jsx
@@ -53,32 +53,47 @@ const Header = ({ scrollLength, currHeight }) => {
     { name: "MBA", pathName: "/programs/mba" },
     { name: "BBA", pathName: "/programs/bba" },
     { name: "B.Com", pathName: "/programs/bcom" },
-    { name: "B.A (Hons) Economics", pathName: "/programs/mba" },
-    { name: "B.A (Hons) English", pathName: "#" },
-    { name: "B.Sc Visual Comumunication", pathName: "/programs/mba" },
-    { name: "B.Sc Animation", pathName: "#" },
-    { name: "M.Sc Visual Comumunication", pathName: "/programs/mba" },
+    { name: "B.A (Hons) Economics", pathName: "/programs/bahonseconomics" },
+    { name: "B.A (Hons) English", pathName: "/programs/bahonsenglish" },
+    {
+      name: "B.Sc Visual Comumunication",
+      pathName: "/programs/bscvisualcomununication",
+    },
+    { name: "B.Sc Animation", pathName: "/programs/bscanimation" },
+    {
+      name: "M.Sc Visual Comumunication",
+      pathName: "/programs/mscvisualcomununication",
+    },
     {
       name: "Diploma in Hotel & Catering Management",
-      pathName: "/programs/mba",
+      pathName: "/programs/diplomahotelcateringmanagement",
     },
 
-    { name: "B.Sc Hotel & Catering Management", pathName: "/programs/mba" },
+    {
+      name: "B.Sc Hotel & Catering Management",
+      pathName: "/programs/bschotelcateringmanagement",
+    },
     {
       name: "B.Sc Food Service Management & Applied Nutrition",
-      pathName: "/programs/mba",
+      pathName: "/programs/bscfoodservicemanagementappliednutrition",
     },
-    { name: "M.Sc Hotel & Catering Management", pathName: "/programs/mba" },
+    {
+      name: "M.Sc Hotel & Catering Management",
+      pathName: "/programs/mschotelcateringmanagement",
+    },
 
     { name: "B.Sc Yoga", pathName: "/programs/bscyoga" },
-    { name: "M.Sc Yoga", pathName: "/programs/mba" },
-    { name: "Diploma Yoga", pathName: "/programs/mba" },
-    { name: "M.A Astrology", pathName: "/programs/mba" },
-    { name: "Diploma in Astrology", pathName: "/programs/mba" },
+    { name: "M.Sc Yoga", pathName: "/programs/mscyoga" },
+    { name: "Diploma Yoga", pathName: "/programs/diplomayoga" },
+    { name: "M.A Astrology", pathName: "/programs/mscastrology" },
+    { name: "Diploma in Astrology", pathName: "/programs/diplomainastrology" },
 
-    { name: "B.A Western Classical Music", pathName: "/programs/mba" },
-    { name: "M.A Karnatic Music", pathName: "/programs/mba" },
-    { name: "M.A Bharatanatyam", pathName: "/programs/mba" },
+    {
+      name: "B.A Western Classical Music",
+      pathName: "/programs/bawesternclassicalmusic",
+    },
+    { name: "M.A Karnatic Music", pathName: "/programs/makarnaticmusic" },
+    { name: "M.A Bharatanatyam", pathName: "/programs/mabharatanatyam" },
   ]);
 
   const [disData, setDisData] = useState([]);
