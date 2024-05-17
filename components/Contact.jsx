e8719234db06ca382886bc22cfb7d6c710d337f5
shan3274 <khanshan949@gmail.com>
2024-05-18 01:10:17 +0530

    Contact data update

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -31,6 +31,137 @@ const Contact = () => {
             <p className="text-[13px]">: 9003058900</p>
           </div>
         </div>
+        <div className="w-full min-h-[40vh] flex items-center justify-around">
+          <div className="w-[300px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">PLACEMENT OFFICER</h1>
+            <p className="text-[13px]">Mr. S. SINGARAVEL, M.E., AP/CIVIL</p>
+            <p className="text-[13px]">
+              C/ST/SCC/SCA SCHOLARSHIP NODAL OFFICER{" "}
+            </p>
+            <p className="text-[13px]">Mr. M KALAINESAN, M.E(PED), MBA., </p>
+            <p className="text-[13px]">K SHILPA, M.E. (STRUCTURAL ENGG.) </p>
+          </div>
+          <div className="w-[200px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">Number</h1>
+            <p className="text-[13px]">: 9786462150</p>
+            <p className="text-[13px]"></p>
+            <p className="text-[13px]">: 9003058900</p>
+            <p className="text-[13px]"></p>
+            <p className="text-[13px]"></p>
+            <p className="text-[13px] mt-10"></p>
+            <p className="text-[13px]"></p>
+            <p className="text-[13px]"></p>
+            <p className="text-[13px]"></p>
+            <p className="text-[13px]">: 9003058900</p>
+          </div>
+        </div>
+        <div className="w-full min-h-[40vh] flex items-center justify-around">
+          <div className="w-[300px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">
+              BC/MBC SCHOLARSHIP NODAL OFFICER
+            </h1>
+            <p className="text-[13px]">Mr. M KALAINESAN, M.E(PED), MBA.,</p>
+          </div>
+          <div className="w-[200px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">Number</h1>
+            <p className="text-[13px]">: 9786462150</p>
+            <p className="text-[13px]"></p>
+          </div>
+        </div>
+        //SSP PORTAL NODAL OFFICER
+        <div className="w-full min-h-[40vh] flex items-center justify-around">
+          <div className="w-[300px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">
+              SSP PORTAL NODAL OFFICER
+            </h1>
+            <p className="text-[13px]">Mr. M KALAINESAN, M.E(PED), MBA., </p>
+          </div>
+          <div className="w-[200px]  flex flex-col items-start justify-center gap-3">
+            <p className="text-[13px]">: 9786462150</p>
+            <p className="text-[13px]"></p>
+          </div>
+        </div>
+        //UMIS PORTAL NODAL OFFICER
+        <div className="w-full min-h-[40vh] flex items-center justify-around">
+          <div className="w-[300px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">
+              UMIS PORTAL NODAL OFFICER
+            </h1>
+            <p className="text-[13px]">Mr. M KALAINESAN, M.E(PED), MBA.,</p>
+          </div>
+          <div className="w-[200px]  flex flex-col items-start justify-center gap-3">
+            <p className="text-[13px]">: 9786462150</p>
+            <p className="text-[13px]"></p>
+          </div>
+        </div>
+        //DOTE ENGINEERING SCHOLARSHIP NODAL OFFICER
+        <div className="w-full min-h-[40vh] flex items-center justify-around">
+          <div className="w-[300px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">
+              DOTE ENGINEERING SCHOLARSHIP NODAL OFFICER
+            </h1>
+            <p className="text-[13px]">Mr. M KALAINESAN, M.E(PED), MBA.,</p>
+          </div>
+          <div className="w-[200px]  flex flex-col items-start justify-center gap-3">
+            <p className="text-[13px]">: 9786462150</p>
+            <p className="text-[13px]"></p>
+          </div>
+        </div>
+        // NAN MUDHALVAN SCHEME SPOC
+        <div className="w-full min-h-[40vh] flex items-center justify-around">
+          <div className="w-[300px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">
+              NAN MUDHALVAN SCHEME SPOC
+            </h1>
+            <p className="text-[13px]">
+              Mrs. K SHILPA, M.E. (STRUCTURAL ENGG.){" "}
+            </p>
+          </div>
+          <div className="w-[200px]  flex flex-col items-start justify-center gap-3">
+            <p className="text-[13px]">:: 8825538889</p>
+            <p className="text-[13px]"></p>
+          </div>
+        </div>
+        //PUDHUMAIPPEN SCHEME NODAL OFFICER
+        <div className="w-full min-h-[40vh] flex items-center justify-around">
+          <div className="w-[300px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">
+              NAN MUDHALVAN SCHEME SPOC
+            </h1>
+            <p className="text-[13px]">Mr. M KALAINESAN, M.E(PED), MBA.,</p>
+          </div>
+          <div className="w-[200px]  flex flex-col items-start justify-center gap-3">
+            <p className="text-[13px]">: 9786462150</p>
+            <p className="text-[13px]"></p>
+          </div>
+        </div>
+        //GOVERNING COUNCIL
+        <div className="w-full min-h-[40vh] flex items-center justify-around">
+          <div className="w-[300px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">
+              NAN MUDHALVAN SCHEME SPOC
+            </h1>
+            <p className="text-[13px]">Mr. M KALAINESAN, M.E(PED), MBA.,</p>
+          </div>
+          <div className="w-[200px]  flex flex-col items-start justify-center gap-3">
+            <p className="text-[13px]">: 9786462150</p>
+            <p className="text-[13px]"></p>
+          </div>
+        </div>
+        //DISCIPLINE AND WELFARE COMMITTEE
+        <div className="w-full min-h-[40vh] flex items-center justify-around">
+          <div className="w-[300px]  flex flex-col items-start justify-center gap-3">
+            <h1 className="text-[20px] font-Comfortaa">
+              NAN MUDHALVAN SCHEME SPOC
+            </h1>
+            <p className="text-[13px]">Mr. M KALAINESAN, M.E(PED), MBA.,</p>
+          </div>
+          <div className="w-[200px]  flex flex-col items-start justify-center gap-3">
+            <p className="text-[13px]">: 9786462150</p>
+            <p className="text-[13px]"></p>
+          </div>
+        </div>
+        //
       </div>
     </div>
   );
